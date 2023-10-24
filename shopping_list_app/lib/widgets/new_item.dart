b0253bf055ea030e76a13ab487b58aa7c195333d
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2023-10-24 18:22:33 +0200

    add save to remote firebase db

--- shopping_list_app/lib/widgets/new_item.dart
+++ shopping_list_app/lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 import '../data/categories.dart';
 import '../models/category.dart';
@@ -20,12 +22,22 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
+
+      final url = Uri.https(
+        'flutter-tuto-e97c6-default-rtdb.europe-west1.firebasedatabase.app',
+        'shopping-list.json',
+      );
+
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title
+        }),
       );
     }
   }
