66638547a54da0b43ac30a493dd34e47773ff13d
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2023-10-25 16:37:40 +0200

    add image background to the detail screen

--- favorite_place_app/lib/screens/place_detail.dart
+++ favorite_place_app/lib/screens/place_detail.dart
@@ -11,17 +11,17 @@ class PlaceDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(place.title),
-      ),
-      body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context)
-              .textTheme
-              .bodyLarge!
-              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          title: Text(place.title),
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            )
+          ],
+        ));
   }
 }
