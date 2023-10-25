13676c072841fc1027354ddbc800e295fa11e426
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2023-10-25 16:31:35 +0200

    add image to place model

--- favorite_place_app/lib/screens/add_place.dart
+++ favorite_place_app/lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:favorite_place_app/providers/user_places.dart';
 import 'package:favorite_place_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -12,14 +14,17 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -43,12 +48,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               decoration: const InputDecoration(labelText: 'Title'),
               controller: _titleController,
               style:
-              TextStyle(color: Theme.of(context).colorScheme.onBackground),
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
             const SizedBox(
               height: 16,
             ),
-            const ImageInput(),
+            ImageInput(onPickImage: (image) {
+              _selectedImage = image;
+            }),
             const SizedBox(
               height: 16,
             ),
