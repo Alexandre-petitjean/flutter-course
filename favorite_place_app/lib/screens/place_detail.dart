036bbfdeee500f5d1defbe2c6b606b4d8b37f79f
Alexandre Petitjean <petitjean.alexandre.pro@gmail.com>
2023-10-26 12:19:04 +0200

    update place detail

--- favorite_place_app/lib/screens/place_detail.dart
+++ favorite_place_app/lib/screens/place_detail.dart
@@ -7,6 +7,12 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C$lat,$lng&key=AIzaSyAMh3IH_fthwZh-DVW-wTbpy5S1pG4ocBw';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +26,36 @@ class PlaceDetailScreen extends StatelessWidget {
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
-            )
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(radius: 70, backgroundImage: NetworkImage(locationImage)),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Colors.black26,
+                          Colors.black54,
+                        ],)
+                    ),
+                    child: Text(place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ));
   }
